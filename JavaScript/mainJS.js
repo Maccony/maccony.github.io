61939f971e457a312ea32766b204aa30a227b0d5
Maccony <maccony@mail.ru>
2020-03-13 16:45:39 +0800

    Update mainJS.js

--- JavaScript/mainJS.js
+++ JavaScript/mainJS.js
@@ -4,7 +4,8 @@ function allLoad(){
         // создадим объект карта (с координатами и zoom) в div элементе
         myMap = new ymaps.Map("loadMap", {center: [12.244444, 109.192778], zoom: 14, controls: ['rulerControl']},
             {suppressMapOpenBlock: true, yandexMapDisablePoiInteractivity: true});
+        document.getElementById("leftGrid").remove;
         document.getElementById("loadMap").style.gridColumn = '1/3';
-        document.getElementById("loadMap").style.gridRow = '2/3';
+        //document.getElementById("loadMap").style.gridRow = '2/3';
     }, false);
 }
