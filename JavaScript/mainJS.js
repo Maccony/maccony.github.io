920391e21c9d875283343bd9df798f7ed4e4a9c1
Maccony <maccony@mail.ru>
2020-03-13 10:20:18 +0800

    обнова

--- JavaScript/mainJS.js
+++ JavaScript/mainJS.js
@@ -1,6 +1,8 @@
 window.onload = allLoad;
 function allLoad(){
-	// создадим объект карта (с координатами и zoom) в div элементе
- 	myMap = new ymaps.Map("loadMap", {center: [12.244444, 109.192778], zoom: 14, controls: ['rulerControl']},
-        {suppressMapOpenBlock: true, yandexMapDisablePoiInteractivity: true});
+    document.getElementById("menu").addEventListener('click', function(e) {
+        // создадим объект карта (с координатами и zoom) в div элементе
+        myMap = new ymaps.Map("loadMap", {center: [12.244444, 109.192778], zoom: 14, controls: ['rulerControl']},
+            {suppressMapOpenBlock: true, yandexMapDisablePoiInteractivity: true});
+    }, false);
 }
