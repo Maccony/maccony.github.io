9a0c138f7f96ad4d95fda4c3ff0b5fe16654da7b
Maccony <maccony@mail.ru>
2020-03-14 09:30:46 +0800

    перекроил

--- JavaScript/mainJS.js
+++ JavaScript/mainJS.js
@@ -1,10 +1,10 @@
 window.onload = allLoad;
 function allLoad(){
-    document.getElementById("NhaTrang").addEventListener('click', function(e) {
-        document.getElementById("leftGrid").remove();
-        document.getElementById("loadMap").style.gridColumn = '1/3';
+    //document.getElementById("NhaTrang").addEventListener('click', function(e) {
+        //document.getElementById("leftGrid").remove();
+        //document.getElementById("loadMap").style.gridColumn = '1/3';
         // создадим объект карта (с координатами и zoom) в div элементе
-        myMap = new ymaps.Map("loadMap", {center: [12.244444, 109.192778], zoom: 14, controls: ['rulerControl']},
-            {suppressMapOpenBlock: true, yandexMapDisablePoiInteractivity: true});
-    }, false);
+        //myMap = new ymaps.Map("loadMap", {center: [12.244444, 109.192778], zoom: 14, controls: ['rulerControl']},
+        //    {suppressMapOpenBlock: true, yandexMapDisablePoiInteractivity: true});
+    //}, false);
 }
