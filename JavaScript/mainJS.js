6388e183503acaf75432c95ce4f462be3a3c8840
Maccony <maccony@mail.ru>
2020-03-18 06:34:07 +0800

    Update mainJS.js

--- JavaScript/mainJS.js
+++ JavaScript/mainJS.js
@@ -1,6 +1,7 @@
 window.onload = allLoad;
 function allLoad(){
     busButton.onclick = function() {
+        alert(this.id);
         if (document.getElementById("main").firstChild == null) {
             // создадим объект карта (с координатами и zoom) в div элементе
  	        myMap = new ymaps.Map("main", {center: [12.244444, 109.192778], zoom: 14, controls: ['rulerControl']},
