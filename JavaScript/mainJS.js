afd3a7c1532a0b071d05ba83f1662eda8f960430
Maccony <maccony@mail.ru>
2020-03-18 05:47:25 +0800

    Update mainJS.js

--- JavaScript/mainJS.js
+++ JavaScript/mainJS.js
@@ -1,10 +1,12 @@
 window.onload = allLoad;
 function allLoad(){
     busButton.onclick = function() {
+        if (document.getElementById("main").firstChild = null) {
         // создадим объект карта (с координатами и zoom) в div элементе
  	    myMap = new ymaps.Map("main", {center: [12.244444, 109.192778], zoom: 14, controls: ['rulerControl']},
-            {suppressMapOpenBlock: true, yandexMapDisablePoiInteractivity: true});
-        alert(document.getElementById("main").firstChild.nodeName);};
+            {suppressMapOpenBlock: true, yandexMapDisablePoiInteractivity: true});}
+        
+        //alert(document.getElementById("main").firstChild.nodeName);};
     //document.getElementById("NhaTrang").addEventListener('click', function(e) {
         //document.getElementById("leftGrid").remove();
         //document.getElementById("loadMap").style.gridColumn = '1/3';
