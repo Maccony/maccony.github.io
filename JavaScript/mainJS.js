36415e486de9edca5285d98e25d28ed61d35d658
Maccony <maccony@mail.ru>
2020-03-18 10:28:36 +0800

    Update mainJS.js

--- JavaScript/mainJS.js
+++ JavaScript/mainJS.js
@@ -1,22 +1,10 @@
 window.onload = allLoad;
-function allLoad(){
-    wrapper.addEventListener('click', function() {
-        alert(this.id);
-        }
-    );
-    //busButton.onclick = function() {
-    //    alert(this.id);
-    //    if (document.getElementById("main").firstChild == null) {
+function allLoad() {
+    busButton.addEventListener('click', function() {
+        if (document.getElementById("main").firstChild == null) {
             // создадим объект карта (с координатами и zoom) в div элементе
- 	//       myMap = new ymaps.Map("main", {center: [12.244444, 109.192778], zoom: 14, controls: ['rulerControl']},
-    //            {suppressMapOpenBlock: true, yandexMapDisablePoiInteractivity: true});}};
-
-        //alert(document.getElementById("main").firstChild.nodeName);};
-    //document.getElementById("NhaTrang").addEventListener('click', function(e) {
-        //document.getElementById("leftGrid").remove();
-        //document.getElementById("loadMap").style.gridColumn = '1/3';
-        // создадим объект карта (с координатами и zoom) в div элементе
-        //myMap = new ymaps.Map("loadMap", {center: [12.244444, 109.192778], zoom: 14, controls: ['rulerControl']},
-        //    {suppressMapOpenBlock: true, yandexMapDisablePoiInteractivity: true});
-    //}, false);
+            myMap = new ymaps.Map("main", {center: [12.244444, 109.192778], zoom: 14, controls: ['rulerControl']},
+                {suppressMapOpenBlock: true, yandexMapDisablePoiInteractivity: true});
+        }
+    });
 }
