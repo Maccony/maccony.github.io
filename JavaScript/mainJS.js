717e8536f9c1eb3aca9254a5032d358bf098c9a8
Maccony <maccony@mail.ru>
2020-03-18 10:57:51 +0800

    события

--- JavaScript/mainJS.js
+++ JavaScript/mainJS.js
@@ -1,10 +1,12 @@
 window.onload = allLoad;
 function allLoad() {
-    busButton.addEventListener('click', function() {
+    var busRoutes = {2: null, 3: null, 4: null, 5: null};
+    busButton.addEventListener('click', function(event) {
         if (document.getElementById("main").firstChild == null) {
             // создадим объект карта (с координатами и zoom) в div элементе
             myMap = new ymaps.Map("main", {center: [12.244444, 109.192778], zoom: 14, controls: ['rulerControl']},
                 {suppressMapOpenBlock: true, yandexMapDisablePoiInteractivity: true});
         }
+        alert(event.target);
     });
 }
