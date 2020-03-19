a362138c7995ef8b64155796365b55dd61bf1131
Maccony <maccony@mail.ru>
2020-03-19 14:53:44 +0800

    Update mainJS.js

--- JavaScript/mainJS.js
+++ JavaScript/mainJS.js
@@ -20,11 +20,14 @@ function allLoad() {
             myMap = new ymaps.Map("main", {center: [52.265374, 104.393120], zoom: 14, controls: ['rulerControl']},
                 {suppressMapOpenBlock: true, yandexMapDisablePoiInteractivity: true});
         }
-        var routeN = event.target.id;
-        if (+routeN > 0) {
-            if (busRoutes[routeN] == null) { busRoutes[routeN] = makeRoute(routeN);
-                myMap.geoObjects.add(busRoutes[routeN]);}
-            else {myMap.geoObjects.remove(busRoutes[routeN]); busRoutes[routeN] = null;}
+        var onHouses = true;
+    	var collectionHouses = new ymaps.GeoObjectCollection({}, {preset: 'islands#blueStretchyIcon'});
+    	collectionHouses.add(new ymaps.Placemark([52.262570, 104.406215], {iconContent: "пеленгатор"}));
+    	collectionHouses.add(new ymaps.Placemark([52.261955, 104.409009], {iconContent: "СКП-297"}));
+
+        if (event.target.id == 'placeMark') {
+            if (onHouses) {myMap.geoObjects.add(collectionHouses); onHouses = false;}
+			else {myMap.geoObjects.remove(collectionHouses); onHouses = true;}
         }
     });
 }
