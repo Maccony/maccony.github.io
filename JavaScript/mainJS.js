a8ce53143c818ec71da5a8b6992a9bf61e89bd9c
Maccony <maccony@mail.ru>
2020-03-19 15:33:27 +0800

    Update mainJS.js

--- JavaScript/mainJS.js
+++ JavaScript/mainJS.js
@@ -1,12 +1,13 @@
 window.onload = allLoad;
 function allLoad() {
+    // создадим объект карта (с координатами и zoom) в div элементе
+    myMap = new ymaps.Map("main", {center: [30.243380, 166.667695], zoom: 14, controls: ['rulerControl']},
+        {suppressMapOpenBlock: true, yandexMapDisablePoiInteractivity: true});
     var busRoutes = {2: null, 3: null, 4: null, 5: null};
     one.addEventListener('click', function(event) {
-        if (document.getElementById("main").firstChild == null) {
-            // создадим объект карта (с координатами и zoom) в div элементе
-            myMap = new ymaps.Map("main", {center: [12.244444, 109.192778], zoom: 14, controls: ['rulerControl']},
-                {suppressMapOpenBlock: true, yandexMapDisablePoiInteractivity: true});
-        }
+        myMap.geoObjects.removeAll();
+        myMap.setCenter([12.244444, 109.192778]);
+
         var routeN = event.target.id;
         if (+routeN > 0) {
             if (busRoutes[routeN] == null) { busRoutes[routeN] = makeRoute(routeN);
